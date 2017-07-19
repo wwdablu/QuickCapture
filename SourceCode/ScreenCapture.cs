e1599756a2b716b75dcb2b0a091630d3d4c8e5f0
Soumya <dev.soumyamob@gmail.com>
2017-07-19 14:29:43 -0400

    Create ScreenCapture.cs

---
+++ SourceCode/ScreenCapture.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace QuikCapture
+{
+    class ScreenCapture
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetDesktopWindow();
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        public static Image CaptureFullscreen()
+        {
+            return Capture(GetDesktopWindow());
+        }
+
+        public static Bitmap CaptureCurrentWindow()
+        {
+            return Capture(GetForegroundWindow());
+        }
+
+        private static Bitmap Capture(IntPtr handle)
+        {
+            var rect = new Rect();
+            GetWindowRect(handle, ref rect);
+            var bounds = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            var result = new Bitmap(bounds.Width, bounds.Height);
+
+            using (var graphics = Graphics.FromImage(result))
+            {
+                graphics.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+            }
+
+            return result;
+        }
+    }
+}
