a0da17c3ad3378d4d5e41793bfeda8fb41bf72a4
Soumya <dev.soumyamob@gmail.com>
2017-07-27 01:20:14 -0400

    Update and rename Form1.cs to QCForm.cs

--- SourceCode/QCForm.cs
+++ SourceCode/QCForm.cs
@@ -6,7 +6,7 @@ using System.IO;
 
 namespace QuickCapture
 {
-    public partial class Form1 : Form
+    public partial class QCForm : Form
     {
         private String saveDirectory = "";
         private int saveCounter = 0;
